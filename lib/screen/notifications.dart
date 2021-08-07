3f83e0a9d974ed12e9426bde0ffdceda79b39235
armnutthapon <armnutthapon@hotmail.co.th>
2021-08-08 01:05:33 +0700

    show notification

--- lib/screen/notifications.dart
+++ lib/screen/notifications.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:little_paw/database/database.dart';
 
@@ -28,10 +29,12 @@ class _NotificationFeedState extends State<NotificationFeed> {
               fontWeight: FontWeight.w500,
               fontFamily: 'Mitr'),
         ),
-        backgroundColor: Colors.blue.withOpacity(1),
+        backgroundColor: Colors.pinkAccent.shade100.withOpacity(1),
         elevation: 10.0,
       ),
-      body: ShowNotification(),
+      body: SingleChildScrollView(
+          child: Column(
+              children: [Container(height: 500, child: ShowNotification())])),
     );
   }
 }
@@ -47,48 +50,147 @@ class _ShowNotificationState extends State<ShowNotification> {
   List data;
 
   getNotificationByID() async {
-    final FirebaseAuth auth = FirebaseAuth.instance;
-    final User userId = auth.currentUser;
-    final String uid = userId.uid;
-
-    http.Response response = await http.get(Uri.parse(
-        'https:littlepaw.herokuapp.com/appointment/notificationByID/$uid'));
-
-    this.setState(() {
-      data = json.decode(response.body);
-    });
-    print(uid);
-    print(data[0]);
-    return data;
-  }
-
-  getNoti() async {
     final FirebaseAuth auth = await FirebaseAuth.instance;
     final User userId = await auth.currentUser;
     final String uid = await userId.uid;
+
     http.Response response =
         await http.get(Uri.parse('$Url/appointment/notificationByID/$uid'));
 
     this.setState(() {
       data = json.decode(response.body);
     });
-
-    print(data);
-    print(Url);
-
-    return print("ID    " + uid);
+    return data;
   }
 
   @override
   void initState() {
     super.initState();
-    getNoti();
+    getNotificationByID();
   }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Container();
+    return ListView.builder(
+      itemCount: data == null ? 0 : data.length,
+      // ignore: missing_return
+      itemBuilder: (BuildContext context, int index) {
+        Color getColor() {
+          if (data[index]['status'] == "Waiting") {
+            return Colors.amber;
+          } else if (data[index]['status'] == "Confirmed") {
+            return Colors.green;
+          } else if (data[index]['status'] == "Denided") {
+            return Colors.red;
+          }
+        }
+
+        if (data[index]['status'] != "Waiting") {
+          print("true");
+          return Container(
+            height: size.height * 0.17,
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              margin: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 3,
+                    blurRadius: 8,
+                    offset: Offset(2, 2), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      data[index]['cid'],
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Mitr'),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      data[index]['symptom'],
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w300,
+                          fontFamily: 'Mitr'),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'สถานะ  :  ',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w300,
+                            fontFamily: 'Mitr'),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: data[index]['status'],
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: getColor(),
+                                fontWeight: FontWeight.w500,
+                                fontFamily: 'Mitr'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child:
+                        Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                      Text(
+                        data[index]['date'],
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Mitr'),
+                      ),
+                      SizedBox(
+                        width: size.width * 0.01,
+                      ),
+                      Text(
+                        data[index]['time_appointment'],
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Mitr'),
+                      ),
+                      SizedBox(
+                        width: size.width * 0.01,
+                      ),
+                      Icon(
+                        FontAwesomeIcons.clock,
+                        color: Colors.blue.shade800,
+                      )
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      },
+    );
   }
 }
